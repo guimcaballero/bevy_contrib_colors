495cde058fd4269d98251dc53c23bb7180fa7778
guimcaballero <guim.caballero@gmail.com>
2020-09-05 17:59:47 +0200

    Add example that prints all tailwind colors

---
+++ examples/main.rs
@@ -0,0 +1,86 @@
+use bevy_contrib_colors::{Colors, Tailwind};
+
+fn main() {
+    let black = Tailwind::BLACK;
+    Colors::print_color(black);
+
+    Colors::print_color(Tailwind::GRAY100);
+    Colors::print_color(Tailwind::GRAY200);
+    Colors::print_color(Tailwind::GRAY300);
+    Colors::print_color(Tailwind::GRAY400);
+    Colors::print_color(Tailwind::GRAY500);
+    Colors::print_color(Tailwind::GRAY600);
+    Colors::print_color(Tailwind::GRAY700);
+    Colors::print_color(Tailwind::GRAY800);
+    Colors::print_color(Tailwind::GRAY900);
+
+    Colors::print_color(Tailwind::RED100);
+    Colors::print_color(Tailwind::RED200);
+    Colors::print_color(Tailwind::RED300);
+    Colors::print_color(Tailwind::RED400);
+    Colors::print_color(Tailwind::RED500);
+    Colors::print_color(Tailwind::RED600);
+    Colors::print_color(Tailwind::RED700);
+    Colors::print_color(Tailwind::RED800);
+    Colors::print_color(Tailwind::RED900);
+
+    Colors::print_color(Tailwind::ORANGE100);
+    Colors::print_color(Tailwind::ORANGE200);
+    Colors::print_color(Tailwind::ORANGE300);
+    Colors::print_color(Tailwind::ORANGE400);
+    Colors::print_color(Tailwind::ORANGE500);
+    Colors::print_color(Tailwind::ORANGE600);
+    Colors::print_color(Tailwind::ORANGE700);
+    Colors::print_color(Tailwind::ORANGE800);
+    Colors::print_color(Tailwind::ORANGE900);
+
+    Colors::print_color(Tailwind::YELLOW100);
+    Colors::print_color(Tailwind::YELLOW200);
+    Colors::print_color(Tailwind::YELLOW300);
+    Colors::print_color(Tailwind::YELLOW400);
+    Colors::print_color(Tailwind::YELLOW500);
+    Colors::print_color(Tailwind::YELLOW600);
+    Colors::print_color(Tailwind::YELLOW700);
+    Colors::print_color(Tailwind::YELLOW800);
+    Colors::print_color(Tailwind::YELLOW900);
+
+    Colors::print_color(Tailwind::TEAL100);
+    Colors::print_color(Tailwind::TEAL200);
+    Colors::print_color(Tailwind::TEAL300);
+    Colors::print_color(Tailwind::TEAL400);
+    Colors::print_color(Tailwind::TEAL500);
+    Colors::print_color(Tailwind::TEAL600);
+    Colors::print_color(Tailwind::TEAL700);
+    Colors::print_color(Tailwind::TEAL800);
+    Colors::print_color(Tailwind::TEAL900);
+
+    Colors::print_color(Tailwind::INDIGO100);
+    Colors::print_color(Tailwind::INDIGO200);
+    Colors::print_color(Tailwind::INDIGO300);
+    Colors::print_color(Tailwind::INDIGO400);
+    Colors::print_color(Tailwind::INDIGO500);
+    Colors::print_color(Tailwind::INDIGO600);
+    Colors::print_color(Tailwind::INDIGO700);
+    Colors::print_color(Tailwind::INDIGO800);
+    Colors::print_color(Tailwind::INDIGO900);
+
+    Colors::print_color(Tailwind::PURPLE100);
+    Colors::print_color(Tailwind::PURPLE200);
+    Colors::print_color(Tailwind::PURPLE300);
+    Colors::print_color(Tailwind::PURPLE400);
+    Colors::print_color(Tailwind::PURPLE500);
+    Colors::print_color(Tailwind::PURPLE600);
+    Colors::print_color(Tailwind::PURPLE700);
+    Colors::print_color(Tailwind::PURPLE800);
+    Colors::print_color(Tailwind::PURPLE900);
+
+    Colors::print_color(Tailwind::PINK100);
+    Colors::print_color(Tailwind::PINK200);
+    Colors::print_color(Tailwind::PINK300);
+    Colors::print_color(Tailwind::PINK400);
+    Colors::print_color(Tailwind::PINK500);
+    Colors::print_color(Tailwind::PINK600);
+    Colors::print_color(Tailwind::PINK700);
+    Colors::print_color(Tailwind::PINK800);
+    Colors::print_color(Tailwind::PINK900);
+}
